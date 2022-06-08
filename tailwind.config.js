add187511ef67b1f4ac2109177fc96356516f0b9
Shorif Ahmed <sashipon001@gmail.com>
2022-06-08 12:02:45 +0600

    Tailwindcss boilerplate

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// const colors = require('tailwindcss/colors');
+// github token: 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
+
+module.exports = {
+  content: [
+    './dist/**/*.html',
+    './src/**/*.{js,jsx,ts,tsx,vue}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
